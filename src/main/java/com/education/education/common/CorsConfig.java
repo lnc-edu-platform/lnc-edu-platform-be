4087e8bd39f0090bfdf53a4642721e173f1b543a
{tgu0925} <{tgu0925@naver.com}>
2026-05-27 14:15:42 +0900

    Cors

--- src/main/java/com/education/education/common/CorsConfig.java
+++ src/main/java/com/education/education/common/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("http://localhost:5174");
 
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
